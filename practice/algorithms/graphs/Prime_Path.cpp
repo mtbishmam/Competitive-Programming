12a73c82f20826e1adb9e791dcd57b3f0c7a957b
mtbishmam <mtbishmam@gmail.com>
2025-08-03 15:11:19 +0600

    BFS Problems

---
+++ practice/algorithms/graphs/Prime_Path.cpp
@@ -0,0 +1,138 @@
+// author: mtbishmam
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+// #include <cassert>
+#include <functional>
+#include <chrono>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+template <typename T> ostream& operator<<(ostream& os, set<T>& s) { for (auto i : s) os << i << " "; return os; }
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, map<A, B>& mp) { for (auto i : mp) os << i << ", "; return os; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll = long long; using ld = long double; using ull = unsigned ll;
+using pii = pair<int, int>; using pli = pair<ll, int>; using pil = pair<int, ll>;
+using pll = pair<ll, ll>; using vl = vector<ll>; using vvl = vector<vl>;
+using vi = vector<int>; using vb = vector<bool>; using vc = vector<char>;
+using vvi = vector<vi>; using vvb = vector<vb>; using vvc = vector<vc>;
+using vpii = vector<pii>; using vpll = vector<pll>; using vs = vector<string>;
+using vvpii = vector<vpii>; using vvvi = vector<vvi>; using vvvl = vector<vvl>;
+using vull = vector<ull>;
+using tiii = tuple<int, int, int>; ; using vtiii = vector<tiii>;
+template <class A, class B> using umap = unordered_map<A, B>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+#define mul(x, y) (((x % MOD) * (y % MOD)) % MOD)
+#define sz(x) (int)(x).size()
+
+const string cq[2] = { "NO", "YES" };
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1 };
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1 };
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int N = 10000;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    auto isprime = [&](int x) {
+        if (x <= 1) return false;
+        bool flag = true;
+        for (int i = 2; i * i <= x; i++)
+            if (x % i == 0) flag = false;
+        return flag;
+        };
+    vi primes;
+    vvi g(N);
+    for (int i = 1000; i <= N; i++) {
+        if (isprime(i)) {
+            primes.eb(i);
+        }
+    }
+    // debug(primes.size()); // 1061
+    for (int i = 0; i < primes.size(); i++) {
+        for (int j = i + 1; j < primes.size(); j++) {
+            string s1 = to_string(primes[i]);
+            string s2 = to_string(primes[j]);
+            int mismatch = 0, sz = min(s1.size(), s2.size());
+            for (int k = 0; k < sz; k++)
+                mismatch += (s1[k] != s2[k]);
+            if (mismatch == 1) {
+                g[primes[i]].eb(primes[j]);
+                g[primes[j]].eb(primes[i]);
+            }
+        }
+    }
+
+    int T(1);
+    cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++) {
+        int x, y;
+        cin >> x >> y;
+        queue<int> q;
+        q.push(x);
+        vb vis(N);
+        vi dis(N, -1);
+        vis[x] = dis[x] = 1;
+        while (q.size()) {
+            int node = q.front();
+            q.pop();
+            for (auto& child : g[node]) {
+                if (!vis[child]) {
+                    vis[child] = 1;
+                    q.push(child);
+                    dis[child] = dis[node] + 1;
+                }
+            }
+        }
+        if (dis[y] == -1) cout << "IMPOSSIBLE";
+        else cout << dis[y] - 1;
+        cout << endl;
+    }
+    return 0;
+}
