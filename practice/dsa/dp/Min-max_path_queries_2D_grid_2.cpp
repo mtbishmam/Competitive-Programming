99140497a526c2b11778f91677159bf63c24e2b8
mtbishmam <mtbishmam@gmail.com>
2025-08-05 08:47:11 +0600

    Add code for Min-Max Path Queries in a 2D Grid

---
+++ practice/dsa/dp/Min-max_path_queries_2D_grid_2.cpp
@@ -0,0 +1,116 @@
+// author: mtbishmam
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+// #include <cassert>
+#include <functional>
+#include <chrono>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+template <typename T> ostream& operator<<(ostream& os, set<T>& s) { for (auto i : s) os << i << " "; return os; }
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, map<A, B>& mp) { for (auto i : mp) os << i << ", "; return os; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll = long long; using ld = long double; using ull = unsigned ll;
+using pii = pair<int, int>; using pli = pair<ll, int>; using pil = pair<int, ll>;
+using pll = pair<ll, ll>; using vl = vector<ll>; using vvl = vector<vl>;
+using vi = vector<int>; using vb = vector<bool>; using vc = vector<char>;
+using vvi = vector<vi>; using vvb = vector<vb>; using vvc = vector<vc>;
+using vpii = vector<pii>; using vpll = vector<pll>; using vs = vector<string>;
+using vvpii = vector<vpii>; using vvvi = vector<vvi>; using vvvl = vector<vvl>;
+using vull = vector<ull>;
+using tiii = tuple<int, int, int>; ; using vtiii = vector<tiii>;
+template <class A, class B> using umap = unordered_map<A, B>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+#define mul(x, y) (((x % MOD) * (y % MOD)) % MOD)
+#define sz(x) (int)(x).size()
+
+const string cq[2] = { "NO", "YES" };
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1 };
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1 };
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 1;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    auto to_bin = [&](int x) {
+        string s;
+        while (x) {
+            s += (x & 1 ? '1' : '0');
+            x /= 2;
+        }
+        reverse(all(s));
+        return s;
+        };
+
+    int T(1);
+    // cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++) {
+        int n, m;
+        cin >> n >> m;
+        vvl a(n, vl(m));
+        for (auto& ai : a) cin >> ai;
+        vvl dp(n, vl(m, INF));
+        function<ll(int, int)> f = [&](int i, int j) {
+            if (i < 0 or j < 0 or i >= n or j >= m) return 1ll * INF;
+            if (i == n - 1) return a[i][j]; // won't find more paths bc there are no neg values 
+            auto& ret = dp[i][j];
+            if (ret != INF) return ret;
+            ret = min(ret, a[i][j] + f(i + 1, j));
+            ret = min(ret, a[i][j] + f(i, j + 1));
+            ret = min(ret, a[i][j] + f(i + 1, j + 1));
+            ret = min(ret, a[i][j] + f(i - 1, j - 1));
+            return ret;
+            };
+        cout << f(0, 0) << endl;
+        // for (auto& dpi : dp) cout << dpi << endl;
+    }
+    return 0;
+}
