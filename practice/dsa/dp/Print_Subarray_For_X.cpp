3b060dbf19f4c11f939c3dd0fe3bc21ea649eb90
mtbishmam <mtbishmam@gmail.com>
2025-08-05 08:14:21 +0600

    Add code for printing subarrays that sum X & Min-Max Path Queries on 2D Grid

---
+++ practice/dsa/dp/Print_Subarray_For_X.cpp
@@ -0,0 +1,125 @@
+// author: mtbishmam
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+// #include <cassert>
+#include <functional>
+#include <chrono>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+template <typename T> ostream& operator<<(ostream& os, set<T>& s) { for (auto i : s) os << i << " "; return os; }
+// template <typename T> ostream& operator<<(ostream &os, const vector<T>& v) { os << '{'; string s; for (const auto &x : v) os << s << x, s = " "; return os << '}'; }
+template <typename A, typename B> istream& operator>>(istream& is, pair<A, B>& i) { return is >> i.ff >> i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+template <typename A, typename B> ostream& operator<<(ostream& os, map<A, B>& mp) { for (auto i : mp) os << i << ", "; return os; }
+// template <typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& i) { return os << '{' << i.ff << ", " << i.ss << '}'; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll = long long; using ld = long double; using ull = unsigned ll;
+using pii = pair<int, int>; using pli = pair<ll, int>; using pil = pair<int, ll>;
+using pll = pair<ll, ll>; using vl = vector<ll>; using vvl = vector<vl>;
+using vi = vector<int>; using vb = vector<bool>; using vc = vector<char>;
+using vvi = vector<vi>; using vvb = vector<vb>; using vvc = vector<vc>;
+using vpii = vector<pii>; using vpll = vector<pll>; using vs = vector<string>;
+using vvpii = vector<vpii>; using vvvi = vector<vvi>; using vvvl = vector<vvl>;
+using vull = vector<ull>;
+using tiii = tuple<int, int, int>; ; using vtiii = vector<tiii>;
+template <class A, class B> using umap = unordered_map<A, B>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+#define mul(x, y) (((x % MOD) * (y % MOD)) % MOD)
+#define sz(x) (int)(x).size()
+
+const string cq[2] = { "NO", "YES" };
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1 };
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1 };
+const int INF = 2147483647;
+const ll LINF = 9223372036854775807;
+const int MOD = 1e9 + 7;
+const int N = 1e5 + 1;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    auto to_bin = [&](int x) {
+        string s;
+        while (x) {
+            s += (x & 1 ? '1' : '0');
+            x /= 2;
+        }
+        reverse(all(s));
+        return s;
+        };
+
+    int T(1);
+    // cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++) {
+        int n, m;
+        cin >> n >> m;
+        vi a(n); cin >> a;
+        vvi dp(n, vi(m + 1, -1));
+        vi move(m + 1);
+        function<int(int, int)> f = [&](int i, int x) {
+            if (x < 0) return 0;
+            if (x == 0) return 1;
+            if (i == n) return 0;
+            if (~dp[i][x]) return dp[i][x];
+            auto& ret = dp[i][x];
+            if (~ret) return ret;
+            ret = 0;
+            int cur = f(i + 1, x - a[i]);
+            if (cur) move[x] = a[i];
+            int cur2 = f(i + 1, x);
+            ret |= cur | cur2;
+            return ret;
+            };
+        int ans_exists = f(0, m);
+        if (ans_exists) {
+            while (m) {
+                cout << move[m] << " ";
+                m -= move[m];
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
