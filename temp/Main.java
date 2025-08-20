5165201878878a6ff30dd6b1c49f33f227e23a24
mtbishmam <mtbishmam@gmail.com>
2025-08-20 11:48:55 +0600

    Updated Main.java

--- temp/Main.java
+++ temp/Main.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean x = sc.nextBoolean();
-        System.out.println(x);
-        System.out.println(!x);
-        sc.close();
+        String s = sc.nextLine();
+        int n = s.length();
+        String temp = "", ans = "";
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                temp = " " + temp;
+                ans = temp + ans;
+                temp = "";
+            } else
+                temp += c;
+        }
+        ans = temp + ans;
+        System.out.println(ans);
     }
 }
