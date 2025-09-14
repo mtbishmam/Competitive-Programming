fabf2422756f81be7f5c0a87732938002aae0ce7
mtbishmam <mtbishmam@gmail.com>
2025-09-14 15:13:04 +0600

    Update 1833E.cpp

--- practice/codeforces/1833E.cpp
+++ practice/codeforces/1833E.cpp
@@ -6,55 +6,45 @@ using vi = vector<int>;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    if (!(cin >> t)) return 0;
-    while (t--) {
+    int T;
+    if (!(cin >> T)) return 0;
+    while (T--) {
         int n; cin >> n;
         vi a(n);
-        for (int i = 0; i < n; ++i) { cin >> a[i]; --a[i]; } // zero-index
-        vector<vector<int>> adj(n);
-        // add undirected edge {i, a[i]} once
+        for (int i = 0; i < n; ++i) { cin >> a[i]; --a[i]; }
+
+        vector<unordered_set<int>> adj(n);
         for (int i = 0; i < n; ++i) {
             int j = a[i];
-            if (i < j) {
-                adj[i].push_back(j);
-                adj[j].push_back(i);
-            }
-            else if (j < i) {
-                // that edge was or will be added when min(i,j) processed
-                // do nothing here
-            }
+            if (i == j) continue; // shouldn't happen per statement
+            adj[i].insert(j);
+            adj[j].insert(i);
         }
+
         vector<char> vis(n, 0);
-        int comp = 0, cyc = 0;
+        int comps = 0, cycles = 0;
         for (int i = 0; i < n; ++i) {
             if (vis[i]) continue;
-            // BFS/DFS to collect component
-            comp++;
-            int nodes = 0;
-            bool allDegTwo = true;
+            comps++;
+            // DFS this component
             stack<int> st;
             st.push(i);
             vis[i] = 1;
+            bool allDegTwo = true;
             while (!st.empty()) {
                 int u = st.top(); st.pop();
-                nodes++;
-                // degree in this undirected simple graph:
-                int deg = adj[u].size();
-                if (deg != 2) allDegTwo = false;
+                if (adj[u].size() != 2) allDegTwo = false;
                 for (int v : adj[u]) {
                     if (!vis[v]) { vis[v] = 1; st.push(v); }
                 }
-                // Note: there might be nodes whose only incident edge was added when
-                // i<ai or ai<i; if a node's edge wasn't added because i>ai, it still
-                // has adjacency via the other side. We added both directions when creating edges.
             }
-            if (allDegTwo) cyc++;
+            if (allDegTwo) ++cycles;
         }
-        int paths = comp - cyc;
-        int mn = cyc + (paths > 0 ? 1 : 0);
-        int mx = comp;
-        cout << mn << " " << mx << "\n";
+
+        int paths = comps - cycles;
+        int mn = cycles + (paths > 0 ? 1 : 0);
+        int mx = comps;
+        cout << mn << " " << mx << '\n';
     }
     return 0;
 }
@@ -64,7 +54,7 @@ int main() {
 /* Lemmas
     1. Complete cycles with length > 2 will always contribute to answer and cannot be compressed
     2. All length 2 cycles can be compressed to 1, and that 1 can be used to combine 2 incomplete cycles
-    3. Incomplete cycles can be compressed
+    3. Incomplete cycles can be compressed with each other as well
 */
 
 /* Solutions
