e300a4e14e4e4a7eb271957795e7e093f41ef4dd
mtbishmam <mtbishmam@gmail.com>
2025-09-19 23:29:49 +0600

    Solve CF 601A

---
+++ practice/codeforces/601A.cpp
@@ -0,0 +1,149 @@
+// author: mtbishmam
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+#include <cassert>
+#include <functional>
+#include <chrono>
+#include <climits>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+#define int long long
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+template <typename T> ostream& operator<<(ostream& os, set<T>& s) { for (auto i : s) os << i << " "; return os; }
+template <typename A, typename B> ostream& operator<<(ostream& os, pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using vi = vector<int>; using vvi = vector<vi>;
+using vl = vector<ll>; using vvl = vector<vl>;
+using vb = vector<bool>; using vvb = vector<vb>;
+using vc = vector<char>; using vvc = vector<vc>;
+using pii = pair<int, int>; using vpii = vector<pii>;
+using pll = pair<ll, ll>; using vpll = vector<pll>;
+using vs = vector<string>;
+using tiii = tuple<int, int, int>; ; using vtiii = vector<tiii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uniq(x) sort(all(x)), (x).erase(unique(all(x)), (x).end())
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+#define mul(x, y) (((x % MOD) * (y % MOD)) % MOD)
+#define sz(x) (int)(x).size()
+
+const string ny[] = { "NO", "YES" };
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1 };
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1 };
+// const int INF = 2147483647;
+// const ll LINF = 9223372036854775807;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+// const int MOD = 998244353;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+const int N = 1e5 + 1;
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    int T(1);
+    // cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++) {
+        int n, m;
+        cin >> n >> m;
+        vvi g1, g2; g1 = g2 = vvi(n + 1);
+        map<pii, bool> mp;
+        for (int i = 0; i < m; i++) {
+            int x, y; cin >> x >> y;
+            if (x > y) swap(x, y);
+            mp[{x, y}] = 1;
+            g1[x].eb(y);
+            g1[y].eb(x);
+        }
+        for (int i = 1; i <= n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (!mp.count({ i, j })) g2[i].eb(j), g2[j].eb(i);
+
+        vi dis(n + 1, INF);
+        auto f = [&](vvi g) {
+            queue<int> q;
+            q.push(1);
+            vb vis(n + 1);
+            vis[1] = 1;
+            dis[1] = 0;
+
+            while (q.size()) {
+                int node = q.front();
+                q.pop();
+                for (auto& child : g[node])
+                    if (!vis[child]) {
+                        vis[child] = 1;
+                        dis[child] = dis[node] + 1;
+                        q.push(child);
+                    }
+            }
+            };
+        if (!mp.count({ 1, n })) {
+            f(g1);
+        }
+        else {
+            f(g2);
+        }
+        int ans = (dis[n] == INF ? -1 : dis[n]);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+//
+
+/* Lemmas
+
+*/
+
+/* Solutions
+
+*/
+
+/* Analysis
+
+*/
