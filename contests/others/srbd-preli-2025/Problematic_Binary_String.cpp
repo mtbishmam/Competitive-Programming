a930844160993bab8f4d4fd24167605fa09d7f0d
mtbishmam <mtbishmam@gmail.com>
2025-10-11 14:16:18 +0600

    Add directory for SRBD Preliminary 2025

---
+++ contests/others/srbd-preli-2025/Problematic_Binary_String.cpp
@@ -0,0 +1,139 @@
+// author: mtbishmam
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <iomanip>
+#include <cstring>
+#include <cassert>
+#include <functional>
+#include <chrono>
+#include <climits>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define em emplace
+#define int int64_t
+
+template <typename T> istream& operator>>(istream& is, vector<T>& a) { for (auto& i : a) is >> i; return is; }
+template <typename T> ostream& operator<<(ostream& os, vector<T>& a) { for (auto& i : a) os << i << " "; return os; };
+template <typename T> ostream& operator<<(ostream& os, set<T>& s) { for (auto i : s) os << i << " "; return os; }
+template <typename A, typename B> ostream& operator<<(ostream& os, pair<A, B>& i) { return os << i.ff << " " << i.ss; }
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using vi = vector<int>; using vvi = vector<vi>;
+using vl = vector<ll>; using vvl = vector<vl>;
+using vb = vector<bool>; using vvb = vector<vb>;
+using vc = vector<char>; using vvc = vector<vc>;
+using pii = pair<int, int>; using vpii = vector<pii>;
+using pll = pair<ll, ll>; using vpll = vector<pll>;
+using vs = vector<string>;
+using tiii = tuple<int, int, int>; ; using vtiii = vector<tiii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define uniq(x) sort(all(x)), (x).erase(unique(all(x)), (x).end())
+#define bug cerr << "!Bugged..." << endl
+#define add(x, y) (x + y >= MOD ? x + y - MOD : x + y)
+#define mul(x, y) (((x % MOD) * (y % MOD)) % MOD)
+#define sz(x) (int)(x).size()
+
+const string ny[] = { "NO", "YES" };
+const int dx[8] = { -1,  0, 0, 1, 1,  1, -1, -1 };
+const int dy[8] = { 0, -1, 1, 0, 1, -1,  1, -1 };
+// const int INF = 2147483647;
+// const ll LINF = 9223372036854775807;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+// const int MOD = 998244353;
+const double EPS = 1e-9;
+const double PI = acos(-1);
+const int N = 1e5 + 1;
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "a", stderr);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    // cout.tie(NULL);
+
+    int T(1);
+    cin >> T;
+    for (int Ti = 1; Ti <= T; Ti++) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        auto get = [&](string t) {
+            int ret = 0, bit = 0;
+            for (int i = 0; i < n - 1; i++) {
+                if (t[i] - '0' != bit) {
+                    t[i] ^= 1;
+                    if (i + 1 < n) t[i + 1] ^= 1;
+                    if (i + 2 < n) t[i + 2] ^= 1;
+                    ret++;
+                }
+                bit ^= 1;
+            }
+            // debug(t);
+            if ((t[n - 1] - '0') != bit) return INF;
+            return ret;
+            };
+        int a, b, c;
+        string t = s;
+        a = get(t);
+
+        t[0] ^= 1;
+        t[1] ^= 1;
+        b = 1 + get(t);
+        c = 1 + get(t);
+        int ans = min({ a, b, c });
+        if (ans == INF) cout << -1;
+        else cout << ans;
+        cout << endl;
+    }
+    return 0;
+}
+
+//
+
+/* Lemmas
+
+*/
+
+/* Solutions
+
+*/
+
+/* Analysis
+
+*/
