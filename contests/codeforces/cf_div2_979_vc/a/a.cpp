74414f5f38454b0d9b5b5fa5aef418168c5a1708
mtbishmam <mtbishmam@gmail.com>
2026-06-01 23:33:13 +0600

    Add directory for Codeforces Div 2 Round 979 Virtual Contest

---
+++ contests/codeforces/cf_div2_979_vc/a/a.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifndef DeBuG
+	#define dbg(...)
+#endif
+
+#define int int64_t
+
+#define sz(x) (int) (x).size()
+#define all(x) begin(x), end(x)
+#define rep(i,a,b) for(int i=a;i<(b);++i)
+using ll = long long; using pii = pair<int,int>;
+using pll = pair<ll,ll>; using vi = vector<int>;
+template<class T> using V = vector<T>;
+
+void solve(int cs) {
+	int n; cin >> n;
+	vi a(n); for (auto& i : a) cin >> i;
+	int ans = 0;
+	int mn = *min_element(all(a));
+	int mx = *max_element(all(a));
+	for (int i = 1; i < n; i++) ans += mx - mn;
+	cout << ans << endl;
+}
+
+signed main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int tc = 1;
+// #ifdef DeBuG
+	cin >> tc;
+// #endif
+	for (int cs = 1; cs <= tc; cs++) solve(cs);
+	return 0;
+}
