3446cb9b22d51d9b5f27bfbfd902a8195c0ec37e
mtbishmam <mtbishmam@gmail.com>
2026-06-13 22:20:57 +0600

    Solved 3 800 rated problems

---
+++ practice/codeforces/A_Second_Order_Statistics.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifndef DeBuG
+	#define dbg(...)
+#endif
+
+#define int int64_t
+
+#define sz(x) (int) (x).size()
+#define all(x) begin(x), end(x)
+#define rep(i,a,b) for(int i=a;i<(b);++i)
+using ll = long long; using pii = pair<int,int>;
+using pll = pair<ll,ll>; using vi = vector<int>;
+template<class T> using V = vector<T>;
+
+void solve(int cs) {
+	int n; cin >> n; 
+	vi a(n); for (auto& i : a) cin >> i;
+	sort(all(a)); a.erase(unique(all(a)), a.end());
+	if (sz(a) > 1) cout << a[1];
+	else cout << "NO";
+}
+
+signed main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int tc = 1;
+// #ifdef DeBuG
+	// cin >> tc;
+// #endif
+	for (int cs = 1; cs <= tc; cs++) solve(cs);
+	return 0;
+}
